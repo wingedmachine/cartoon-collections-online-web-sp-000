110e6723bc7e8216f038580f39eb1d4c8e40eeca
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 23:18:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(powers)
+  powers.map do |power|
+    power = power[0].upcase + power[1..-1] + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(powers)
+  powers.any? { |power| power.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_indices = []
+  cheese_types.each do |cheese|
+    if ingredients.include?(cheese)
+      cheese_indices << ingredients.index(cheese)
+    end
+  end
+  cheese_indices.size > 0 ? ingredients[cheese_indices.min] : nil
 end
