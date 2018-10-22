cf446d05f44693d88f80268fb76d78f4b09f3453
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 23:12:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(powers)
   powers.any? { |power| power.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
